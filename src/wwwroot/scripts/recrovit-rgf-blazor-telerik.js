b01444b799fec1b800b33b7aebbd8680ef93f0f9
Tamás Mikola <mikola.tamas@recrovit.hu>
2023-11-09 15:16:22 +0100

    Add project files.

---
+++ src/wwwroot/scripts/recrovit-rgf-blazor-telerik.js
@@ -0,0 +1,19 @@
+﻿/*!
+* recrovit-rgf-blazor-telerik.js v1.0.0
+*/
+
+window.Recrovit = window.Recrovit || { RGF: {} };
+window.Recrovit.RGF.Blazor = window.Recrovit.RGF.Blazor || {};
+var Blazor = window.Recrovit.RGF.Blazor;
+
+Blazor.TelerikUI = {
+    Grid: {
+        initializeTable: function (dotNetObj, entityDomId) {
+            var entity = document.querySelector(`#${entityDomId}`);
+            var table = entity.querySelector('table.k-grid-table.k-table');
+            table.classList.add('recro-grid');
+            table = entity.querySelector('table.k-grid-header-table.k-table');
+            table.classList.add('recro-grid');
+        }
+    }
+};
