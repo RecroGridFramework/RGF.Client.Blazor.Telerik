70625edf757c9149bc819a577ea8691417b2e29a
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-02-03 16:16:37 +0100

    EventDispatcher

--- src/Components/GridComponent.razor.cs
+++ src/Components/GridComponent.razor.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
+using Recrovit.RecroGridFramework.Abstraction.Contracts.Services;
 using Recrovit.RecroGridFramework.Abstraction.Infrastructure.Events;
 using Recrovit.RecroGridFramework.Abstraction.Models;
 using Recrovit.RecroGridFramework.Client.Blazor.Components;
+using Recrovit.RecroGridFramework.Client.Blazor.Events;
 using Recrovit.RecroGridFramework.Client.Handlers;
-using System;
-using System.Linq;
 using Telerik.Blazor.Components;
 using Telerik.DataSource;
 
@@ -39,8 +39,8 @@ public partial class GridComponent : ComponentBase, IDisposable
     protected override void OnInitialized()
     {
         base.OnInitialized();
-        GridParameters.Events.CreateAttributes.Subscribe(OnCreateAttributes);
-        GridParameters.Events.ColumnSettingsChanged.Subscribe((arg) => Recreate());
+        GridParameters.EventDispatcher.Subscribe(RgfGridEventKind.CreateAttributes, OnCreateAttributes);
+        GridParameters.EventDispatcher.Subscribe(RgfGridEventKind.ColumnSettingsChanged, (arg) => Recreate());
         _initialized = true;
     }
 
@@ -141,11 +141,12 @@ public partial class GridComponent : ComponentBase, IDisposable
         }
     }
 
-    protected virtual Task OnCreateAttributes(DataEventArgs<RgfDynamicDictionary> rowData)
+    protected virtual Task OnCreateAttributes(IRgfEventArgs<RgfGridEventArgs> arg)
     {
+        var rowData = arg.Args.RowData ?? throw new ArgumentException();
         foreach (var prop in EntityDesc.SortedVisibleColumns)
         {
-            var attr = rowData.Value["__attributes"] as RgfDynamicDictionary;
+            var attr = rowData["__attributes"] as RgfDynamicDictionary;
             if (attr != null)
             {
                 string? propAttr = null;
