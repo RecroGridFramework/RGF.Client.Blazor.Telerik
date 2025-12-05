0becc735645cca5d4c61de42622624f14b49a82e
Tamás Mikola <mikola.tamas@recrovit.hu>
2025-12-05 09:52:27 +0100

    -Upgraded the project target to .NET 10. -Update for Telerik.UI.for.Blazor 12 compatibility. -Updated Recrovit.RecroGridFramework dependencies to version 10.

--- src/Configuration.cs
+++ src/Configuration.cs
@@ -8,7 +8,7 @@ namespace Recrovit.RecroGridFramework.Client.Blazor.TelerikUI;
 
 public static class RGFClientBlazorTelerikConfiguration
 {
-    public static async Task InitializeRgfTelerikUIAsync(this IServiceProvider serviceProvider, string themeName = "kendo-theme-default/all", bool trial = false, bool loadResources = true)
+    public static async Task InitializeRgfTelerikUIAsync(this IServiceProvider serviceProvider, string themeName = "kendo-theme-default/all", bool loadResources = true)
     {
         RgfBlazorConfiguration.RegisterComponent<MenuComponent>(RgfBlazorConfiguration.ComponentType.Menu);
         RgfBlazorConfiguration.RegisterComponent<DialogComponent>(RgfBlazorConfiguration.ComponentType.Dialog);
@@ -17,19 +17,18 @@ public static class RGFClientBlazorTelerikConfiguration
         if (loadResources)
         {
             var jsRuntime = serviceProvider.GetRequiredService<IJSRuntime>();
-            await LoadResourcesAsync(jsRuntime, themeName, trial);
+            await LoadResourcesAsync(jsRuntime, themeName);
         }
 
         await serviceProvider.InitializeRGFBlazorApexChartsAsync(loadResources);
     }
 
-    public static async Task LoadResourcesAsync(IJSRuntime jsRuntime, string themeName, bool trial = false)
+    public static async Task LoadResourcesAsync(IJSRuntime jsRuntime, string themeName)
     {
         var libName = Assembly.GetExecutingAssembly().GetName().Name;
-        string trialEx = trial ? ".Trial" : "";
-        await jsRuntime.InvokeVoidAsync("import", $"{RgfClientConfiguration.AppRootPath}/_content/Telerik.UI.for.Blazor{trialEx}/js/telerik-blazor.js");
+        await jsRuntime.InvokeVoidAsync("import", $"{RgfClientConfiguration.AppRootPath}/_content/Telerik.UI.for.Blazor/js/telerik-blazor.js");
         await jsRuntime.InvokeVoidAsync("eval", "document.getElementsByTagName('body')[0].classList.add('k-body');");
-        await jsRuntime.InvokeVoidAsync("Recrovit.LPUtils.AddStyleSheetLink", $"{RgfClientConfiguration.AppRootPath}/_content/Telerik.UI.for.Blazor{trialEx}/css/{themeName}.css", false, TelerikThemeId);
+        await jsRuntime.InvokeVoidAsync("Recrovit.LPUtils.AddStyleSheetLink", $"{RgfClientConfiguration.AppRootPath}/_content/Telerik.UI.for.Blazor/css/{themeName}.css", false, TelerikThemeId);
         await jsRuntime.InvokeVoidAsync("Recrovit.LPUtils.AddStyleSheetLink", $"{RgfClientConfiguration.AppRootPath}/_content/{libName}/css/telerikui-styles.css", false, BlazorTelerikUiCss);
 
         await jsRuntime.InvokeVoidAsync("import", $"{RgfClientConfiguration.AppRootPath}/_content/{libName}/scripts/recrovit-rgf-blazor-telerik.js");
